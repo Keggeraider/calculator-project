8a4ce527faf97a9da8af361e9eb959074ed92b3b
Kegeraider <brian@pugnoni.us>
2026-06-17 16:17:32 -0500

    add grid to buttons for better styling

--- static/js/app.js
+++ static/js/app.js
@@ -3,10 +3,57 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
-let a = 0;
-let b = 0;
-let operator = ["+", "-", "*", "/"];
+let newNum = 0;
+let operator = "";
 
-function operate(a, b) {
+const display = document.querySelector('.calcDisplay');
+const digits = document.querySelectorAll('.digits button');
+const operators = document.querySelectorAll('.operations button');
 
+digits.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        console.log(e.target.textContent);
+        newNum = e.target.textContent;
+        display.textContent = newNum;
+        console.log("newNum = ", newNum);
+    });
+});
+/*numB = digits.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            console.log(e.target.textContent);
+            numB = e.target.textContent;
+            display.textContent = numB;
+            console.log("numB = ", numB);
+        });
+    });*/
+
+
+function getOperation() {
+    operators.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            console.log(e.target.textContent);
+            operator = e.target.textContent;
+            display.textContent = operator;
+        });
+    });
+}
+
+
+
+// operator = getOperation();
+
+function operate() {
+    let result = 0;
+
+    if (operator === "+") {
+        result = add(numA, numB);
+    } else if (operator === "-") {
+        result = subtract(numA, numB);
+    } else if (operator === "*") {
+        result = multiply(a,b);
+    } else if (operator === "&divide;") {
+        result = divide(a,b);
+    }
+
+    display.textContent = result;
 }
