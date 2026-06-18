ae00a27f3d48f81be9b5c25de6af23fa6efc4724
Brian Pugnali <brian@pugnoni.us>
2026-06-18 10:14:09 -0500

    remove operator display

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,14 @@
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
 
 let input = [];
 let a = null;
 let b = null;
 let calcDisplay = "";
 let operator = [];
+let result = 0;
 
 const display = document.querySelector('.calcDisplay');
 const buttons = document.querySelectorAll('button');
@@ -15,11 +16,13 @@ const operators = document.querySelectorAll('.operations button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        if (e.target.parentNode.className === "digits" && (e.target.id !== "ac" || e.target.id !== "compute")) {
+        if (e.target.parentNode.className === "digits" && (e.target.id !== "ac" && e.target.id !== "compute")) {
             input.push(e.target.textContent);
-            calcDisplay += e.target.textContent;
+            if (calcDisplay == 0) {
+                calcDisplay = e.target.textContent;
+            } else {calcDisplay += e.target.textContent;}
+
         } else if  (e.target.parentNode.className === "operators") {
-            calcDisplay = e.target.textContent;
             operator.push(e.target.textContent);
             if (a === null) {
                 a = input.shift();
@@ -31,10 +34,24 @@ buttons.forEach((button) => {
                 while (input.length > 0) {
                     b += input.shift()
                 }
+            } else if (a !== null && b !== null) {
+                operate();
+                calcDisplay = result;
+                a = result;
+                b = null;
             }
         } else if (e.target.id === "compute") {
-
+            operate();
+            calcDisplay = result;
+        } else if (e.target.id === "ac"){
+            a = null;
+            b = null;
+            input = [];
+            operator = [];
+            calcDisplay = "0";
+            result = null;
         }
+ 
         display.textContent = calcDisplay;
         console.log(e.target.parentNode.className);
         console.log(e.target.id);
@@ -46,16 +63,15 @@ buttons.forEach((button) => {
 });
 
 function operate() {
-    let result = 0;
-
-    if (operator === "+") {
-        result = add(numA, numB);
-    } else if (operator === "-") {
-        result = subtract(numA, numB);
-    } else if (operator === "*") {
-        result = multiply(a,b);
-    } else if (operator === "&divide;") {
-        result = divide(a,b);
+    let symbol = operator.shift();
+    if (symbol === "+") {
+        result = add(a, b);
+    } else if (symbol === "-") {
+        result = subtract(a, b);
+    } else if (symbol === "*") {
+        result = multiply(a, b);
+    } else if (symbol === "&divide;") {
+        result = divide(a, b);
     }
 
     display.textContent = result;
